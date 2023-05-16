b7fd47042299966638c72199ccf9025d9e01332d
Sowmiyaa Vasan <sowmiyaavasan@Sowmiyaas-MacBook-Pro.local>
2023-05-16 13:49:56 -0700

    connecting backend and frontend

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ChatPage = () => {
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chat");
+    setChats(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  const [chats, setChats] = useState([]);
+
   return (
     <div>
-      Chat Page!
+      {chats.map((chat) => (
+        <div key={chat._id}>{chat.chatName}</div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default ChatPage;
