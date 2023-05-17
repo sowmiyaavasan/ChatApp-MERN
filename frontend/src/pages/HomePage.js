a32ff93cfa01f40106d8b9a1870071bb6b890eea
Sowmiyaa Vasan <sowmiyaavasan@Sowmiyaas-MacBook-Pro.local>
2023-05-17 10:59:37 -0700

    added components for login and signup

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Center,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../components/authentication/Login";
+import Signup from "../components/authentication/Signup";
 
 const HomePage = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" as="b" fontFamily="Work sans">
+          <Center>Talk-A-Tive</Center>
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded" colorScheme="cyan">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
