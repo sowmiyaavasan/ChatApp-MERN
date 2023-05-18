b0a2333796ed564366bb224ce558c32fa694e562
Sowmiyaa Vasan <sowmiyaavasan@Sowmiyaas-MacBook-Pro.local>
2023-05-18 15:11:45 -0700

    submitting data after user login and signup to backend

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -8,32 +8,87 @@ import {
   Button,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   const [show, setShow] = useState(false);
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  //  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const history = useHistory();
 
   const handleClick = () => {
     setShow(!show);
   };
 
-  const submitHandler = () => {};
+  //submitting data to backend
+  const submitHandler = async () => {
+    // setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      // setLoading(false);
+      return;
+    }
+
+    // console.log(email, password);
+    try {
+      //submitting data
+      const config = {
+        //defining header for data getting submitted
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+
+      // console.log(JSON.stringify(data));
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      //storing the submitted data in local
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      // setLoading(false);
+      history.push("/chats"); //user taken to chats page once login is successful
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      // setLoading(false);
+    }
+  };
 
   return (
     <VStack spacing="5px">
-      <FormControl id="first-name" isRequired>
-        <FormLabel>Name</FormLabel>
-        <Input
-          placeholder="Enter your name"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </FormControl>
       <FormControl id="email" isRequired>
         <FormLabel>Email ID</FormLabel>
         <Input
           placeholder="Enter your email id"
+          //to display email on input field when login as guest is selected
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
       </FormControl>
@@ -43,6 +98,8 @@ const Login = () => {
           <Input
             type={show ? "text" : "password"}
             placeholder="Enter your password"
+            //to display password on input field when login as guest is selected
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           <InputRightElement width="4.5rem">
@@ -57,6 +114,7 @@ const Login = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        // isLoading={loading}
       >
         Login
       </Button>
