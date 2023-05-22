e8a0a9a2dfd7f41d5a0f994378710014f2af4b0a
Sowmiyaa Vasan <sowmiyaavasan@Sowmiyaas-MacBook-Pro.local>
2023-05-22 15:44:23 -0700

    added API for adding/removing user from group

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,6 +5,8 @@ const {
   fetchChats,
   createGroupChat,
   renameGroup,
+  removeFromGroup,
+  addToGroup,
 } = require("../controllers/chatControllers");
 
 const router = express.Router();
@@ -14,8 +16,11 @@ router.route("/").post(protect, accessChat); //chat creation
 router.route("/").get(protect, fetchChats); //fetch all chats
 
 router.route("/group").post(protect, createGroupChat); //create group chat
+
 router.route("/rename").put(protect, renameGroup); //rename a group
-// router.route("/removegroup").put(protect, removeFromGroup);
-// router.route("/addgroup").put(protect, addToGroup);
+
+router.route("/removegroup").put(protect, removeFromGroup); //remove user from a group
+
+router.route("/addgroup").put(protect, addToGroup); //add user to a group
 
 module.exports = router;
