ea1d44787fff3d5690995b7379e54b5ad5419dd4
Sowmiyaa Vasan <sowmiyaavasan@Sowmiyaas-MacBook-Pro.local>
2023-05-25 13:48:54 -0700

    chats added for searched user when selected

--- frontend/src/components/chat/MyChats.js
+++ frontend/src/components/chat/MyChats.js
@@ -1,7 +1,116 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { ChatState } from "../../context/ChatProvider";
+import { useToast, Box, Button, Text, Stack } from "@chakra-ui/react";
+import axios from "axios";
+import { getSender } from "../../config/ChatLogics";
+import { AddIcon } from "@chakra-ui/icons";
+import ChatLoading from "./ChatLoading";
 
 const MyChats = () => {
-  return <div>My Chats</div>;
+  const [loggedUser, setLoggedUser] = useState();
+  const { user, selectedChat, setSelectedChat, chats, setChats } = ChatState();
+  const toast = useToast();
+
+  const fetchChats = async () => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.get("/api/chat", config);
+      setChats(data);
+      // console.log(data);
+    } catch (error) {
+      toast({
+        title: "Error occured!",
+        description: "Failed to load the results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+
+  useEffect(() => {
+    setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
+    fetchChats();
+  }, []);
+
+  return (
+    <Box
+      //base - smaller screens, md - medium screens and above
+      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir="column"
+      alignItems="center"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Box
+        pb={3}
+        px={3}
+        fontSize={{ base: "25px", md: "20px" }}
+        fontFamily="Work sans"
+        display="flex"
+        w="100%"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        My Chats
+        <Button
+          display="flex"
+          fontSize={{ base: "15px", md: "7px", lg: "10px" }}
+          rightIcon={<AddIcon />}
+        >
+          New Group Chat
+        </Button>
+      </Box>
+      <Box
+        display="flex"
+        flexDir="column"
+        p={3}
+        bg="#F8F8F8"
+        w="100%"
+        h="100%"
+        borderRadius="lg"
+        overflowY="hidden"
+      >
+        {chats ? (
+          <Stack overflowY="scroll">
+            {chats.map((chat) => (
+              <Box
+                onClick={() => setSelectedChat(chat)}
+                cursor="pointer"
+                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                color={selectedChat === chat ? "white" : "black"}
+                px={3}
+                py={2}
+                borderRadius="lg"
+                key={chat._id}
+              >
+                <Text>
+                  {/* if not a group chat, set the chat name as the recieving
+                  user's name */}
+                  {!chat.isGroupChat
+                    ? getSender(loggedUser, chat.users)
+                    : chat.chatName}{" "}
+                  {/* otherwise set the chat name as group name */}
+                </Text>
+              </Box>
+            ))}
+          </Stack>
+        ) : (
+          <ChatLoading />
+        )}
+      </Box>
+    </Box>
+  );
 };
 
 export default MyChats;
